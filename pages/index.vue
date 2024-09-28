676400ca14d7e2c0cf7b55c03ccf6e9fbb0ee533
wattanx <wattan.dev@gmail.com>
2024-09-28 16:55:13 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
-import NavigationCard from '~/components/NavigationCard.vue';
-</script>
-
 <template>
-  <div class="flex flex-col gap-2">
-    <NavigationCard href="/nuxt-content-sc" title="Nuxt Content with Server Components" description="Without Server Components" />
+  <div class="overflow-auto">
+    <StaticMarkdownRender path="/example" />
   </div>
 </template>
+
+<style scoped>
+:deep(h1, h2, h3, h4) {
+  @apply mb-1 mt-10 font-semibold;
+}
+
+:deep(h2 a) {
+  @apply text-inherit;
+}
+
+:deep(h1) {
+  @apply text-2xl;
+}
+
+:deep(p) {
+  @apply my-4;
+}
+
+:deep(pre) {
+  @apply my-4 rounded-lg p-4 text-sm;
+}
+
+:deep(a) {
+  @apply text-blue-400;
+}
+</style>
