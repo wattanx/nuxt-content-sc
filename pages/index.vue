7602e5ba68be2815733a321338e8c149a3c87c4a
wattanx <wattan.dev@gmail.com>
2024-09-28 16:50:49 +0900

    fix: remove page

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,6 @@ import NavigationCard from '~/components/NavigationCard.vue';
 
 <template>
   <div class="flex flex-col gap-2">
-    <NavigationCard
-      href="/nuxt-content"
-      title="Nuxt Content with Server Components"
-      description="Combine Nuxt Content with Server Components"
-    />
     <NavigationCard href="/nuxt-content-sc" title="Nuxt Content with Server Components" description="Without Server Components" />
   </div>
 </template>
