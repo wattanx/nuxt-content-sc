918043a67679ca92ad88284d157c12650f7ce24d
wattanx <wattan.dev@gmail.com>
2024-10-06 23:43:52 +0900

    refactor: replace to sc

--- pages/without-content.vue
+++ pages/without-content.vue
@@ -1,38 +1,3 @@
-<script setup lang="ts">
-import sanitizeHtml from 'sanitize-html';
-import { marked } from 'marked';
-const { data } = await useFetch('/api/example');
-
-const html = await marked(data.value || '');
-const sanitizedHtml = sanitizeHtml(html);
-</script>
-
 <template>
-  <div v-html="sanitizedHtml"></div>
+  <StaticMdRender />
 </template>
-
-<style scoped>
-:deep(h1, h2, h3, h4) {
-  @apply mb-1 mt-10 font-semibold;
-}
-
-:deep(h2 a) {
-  @apply text-inherit;
-}
-
-:deep(h1) {
-  @apply text-2xl;
-}
-
-:deep(p) {
-  @apply my-4;
-}
-
-:deep(pre) {
-  @apply my-4 rounded-lg p-4 text-sm;
-}
-
-:deep(a) {
-  @apply text-blue-400;
-}
-</style>
